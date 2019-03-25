73d51163303fd4745c35e2d2addf813e5cf5b781
stg25 <stefanoghizzi@yahoo.com>
2019-03-25 17:16:10 +0100

    Milestone 1 OK

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-// add movie info: title and popularity
+//  add movies + info: title - originalTitle - originalLanguage - averageVote
 
-function addMovieInfo(title, originalTitle, originalLanguage, averageVote) {
+function addMovies(title, originalTitle, originalLanguage, averageVote) {
   var tempData = {
 
     title: title,
@@ -16,11 +16,56 @@ function addMovieInfo(title, originalTitle, originalLanguage, averageVote) {
   var finalHTML = compiled(tempData);
   var container = $(".film-container");
   container.append(finalHTML);
+
+  var myContainer = $(".container");
+  myContainer.animate({ scrollTop: myContainer.prop("scrollHeight")});  // not working yet
 }
 
-// ajax test
+//  movie search with BUTTON
 
-function movieSearch(title) {
+function movieButtonSearch() {
+
+  var input = $("input");
+  var title = input.val().toLowerCase();
+
+  var outData = {
+    api_key: "d9a3a396cd57a78d96bf9dfa9321f9b2",
+    language: "it-IT",
+    query: title
+  }
+
+  $.ajax({
+    url: "https://api.themoviedb.org/3/search/movie",
+    method: "GET",
+    data: outData,
+
+    success: function (data) {
+
+      var results = data.results;
+      for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+        var title = result.title;
+        var originalTitle = result.original_title;
+        var originalLanguage = result.original_language;
+        var averageVote = result.vote_average;
+
+        addMovies(title, originalTitle, originalLanguage, averageVote);
+      }
+    },
+
+    error: function (rquest, state, error) {
+      console.log("AJAX ERROR");
+    }
+  });
+
+  clearInput();
+}
+
+//  movie search with KEYUP - work in progress
+
+function movieKeyupSearch() {
+  var me = $(this);
+  var title = me.val().toLowerCase();
 
   var outData = {
     api_key: "d9a3a396cd57a78d96bf9dfa9321f9b2",
@@ -45,13 +90,25 @@ function movieSearch(title) {
 
         addMovieInfo(title, originalTitle, originalLanguage, averageVote);
       }
-
     },
 
     error: function (rquest, state, error) {
-
+      console.log("AJAX ERROR");
     }
   });
+}
+
+//  clear input function
+
+function clearInput() {
+  var input = $("input");
+  input.val("");
+}
+
+//  clear films function - wprk in progress
+
+function clearFilms() {
+  var films = $(".film-container");
 }
 
 // init function
@@ -59,61 +116,12 @@ function movieSearch(title) {
 function init() {
   var doc = $(document);
 
-  doc.on("click", "button", function () {
-    movieSearch("back in the future")
-  })
+  // search movie BUTTON
+  doc.on("click", "button", movieButtonSearch)
 
+  // search movie KEYUP
+  // doc.on("keyup", "input", movieKeyupSearch);
 
 }
 
 $(document).ready(init);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Finish
